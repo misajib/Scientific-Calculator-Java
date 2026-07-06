3a8ad47d7485836772cff24f20e46bee90618f55
Muhammad Mohaiminul Islam  Sajib <misajib0493@gmail.com>
2026-07-07 03:46:44 +0600

    Initial commit

---
+++ src/sc_calculator_sajib/btn0.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sc_calculator_sajib;
+
+/**
+ *
+ * @author Md Mohaiminul Islam Sajib (232311314)
+ */
+class btn0 {
+    
+}
---
+++ src/sc_calculator_sajib/inputText.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sc_calculator_sajib;
+
+/**
+ *
+ * @author ASUS
+ */
+public class inputText {
+
+    static void setLength(int i) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
